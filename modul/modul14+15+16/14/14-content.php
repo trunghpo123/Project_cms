21b9631f4ecc43321d7f32ad5e1c24d5ca008aa4
trieuppp01230 <trieunguyencd17tt10@gmail.com>
2019-10-29 10:59:14 +0700

    ff

---
+++ modul/modul14+15+16/14/14-content.php
@@ -0,0 +1,138 @@
+<?php
+$url_host = $_SERVER['HTTP_HOST'];
+
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+
+$url_path = $url_host . $matches[1][0];
+
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-14">
+    <div class="banner-inner">
+        <div class="banner">
+        </div>
+        <div class="overlay"></div>
+        <div class="text-inner">
+            <h1 class="lead">About Us</h1>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat….</p>
+        </div>
+    </div>
+    <div class="content">
+        <div class="container">
+            <div class="row clearfix">
+                <div class="col-md-12">
+                    <p class="text"><strong>Lorem ipsum</strong> dolor sit amet, consectetur adipiscing elit. Morbi fermentum justo vitae convallis varius. Nulla tristique risus ut justo pulvinar mattis. Phasellus aliquet egestas mauris in venenatis. Nulla tristique risus ut justo pulvinar mattis. Phasellus aliquet egestas mauris in venenatis.</p>
+                    <div class="is-divider"></div> 
+                </div>
+                <div class="col-md-3">
+                    <div class="col-inner">
+                        <div class="box-image">
+                            <img src="images/img1.jpg" alt="">
+                        </div>
+                        <div class="box-text">
+                            <div class="box-text-inner">
+                                <h4 class="uppercase">
+                                    <span class="person-name">Troy Gray</span><br>
+                                    <span class="person-title">CEO / Founder</span>
+                                </h4>
+                                <div class="social-icons">
+                                    <div class="icon-inner">
+                                        <a href="#" class="icon"><i class="fa fa-facebook"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-twitter"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-envelope"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-pinterest"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-linkedin"></i></a>
+                                    </div>
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="col-inner">
+                        <div class="box-image">
+                            <img src="images/img2.jpg" alt="">
+                        </div>
+                        <div class="box-text">
+                            <div class="box-text-inner">
+                                <h4 class="uppercase">
+                                    <span class="person-name">Richy Lace</span><br>
+                                    <span class="person-title">Marketing Director</span>
+                                </h4>
+                                <div class="social-icons">
+                                    <div class="icon-inner">
+                                        <a href="#" class="icon"><i class="fa fa-facebook"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-twitter"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-envelope"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-pinterest"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-linkedin"></i></a>
+                                    </div>
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="col-inner">
+                        <div class="box-image">
+                            <img src="images/img3.jpg" alt="">
+                        </div>
+                        <div class="box-text">
+                            <div class="box-text-inner">
+                                <h4 class="uppercase">
+                                    <span class="person-name">Jane Gray</span><br>
+                                    <span class="person-title">Public Relations</span>
+                                </h4>
+                                <div class="social-icons">
+                                    <div class="icon-inner">
+                                        <a href="#" class="icon"><i class="fa fa-facebook"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-twitter"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-envelope"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-pinterest"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-linkedin"></i></a>
+                                    </div>
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="col-inner">
+                        <div class="box-image">
+                            <img src="images/img4.jpg" alt="">
+                        </div>
+                        <div class="box-text">
+                            <div class="box-text-inner">
+                                <h4 class="uppercase">
+                                    <span class="person-name">July Wood</span><br>
+                                    <span class="person-title">Customer Support</span>
+                                </h4>
+                                <div class="social-icons">
+                                <div class="social-icons">
+                                    <div class="icon-inner">
+                                        <a href="#" class="icon"><i class="fa fa-facebook"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-twitter"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-envelope"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-pinterest"></i></a>
+                                        <a href="#" class="icon"><i class="fa fa-linkedin"></i></a>
+                                    </div>
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+    
+
---
+++ modul/modul2/2/2.php
@@ -0,0 +1,48 @@
+
+<?php
+
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+
+    if (!class_exists('lessc')) {
+        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
+        require_once($dir_block.'/libs/lessc.inc.php');
+    }
+    
+    $less = new lessc;
+    $less->compileFile('less/2.less', 'css/2.css');
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>2</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/2.css" rel="stylesheet" type="text/css"/>
+        <script src="js/jquery-2.1.4.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/script.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Dancing+Script&display=swap" rel="stylesheet">
+        <link href="../2/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/2.less', 'css/2.css');
+        ?>
+        <link href="../2/css/2.css" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>   
+            <?php include '2-content.php'; ?>
+
+    </body>
+
+</html>
