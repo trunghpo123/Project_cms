524bb49e737fd88ccacc4ad1e2582412ff540695
trungcap <ctxtrung@gmail.com>
2019-11-03 20:39:17 +0700

    f

---
+++ modul/10/10-content.php
@@ -0,0 +1,138 @@
+<div class="type-2">
+    <div class="container">
+        <nav id="fixNav">
+            <ul>
+                <li class="dropdownn" id="hh"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Demos<span
+                            class="caret"></span></a>
+                    <div class="col-md-3">
+                        <ul class="dropdown-menu">
+                            <div class="col-md-4">
+                                <li class="cot-menu"><a id="border" href="#">Shop Demos</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Classic Shop</a></li>
+                                <li class="cot-menu"><a  id="border" href="#">Simple Slider</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Fullscreen Fashion</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Video Cover</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Slider Cover</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Grid Style 1</a></li>
+                                <li class="cot-menu"><a  id="border" href="#">Grid Style 2</a></li>
+                                <li class="cot-menu"><a  href="#">Grid Style 3</a></li>
+                            </div>
+                            <div class="col-md-4">
+                                <li class="cot-menu"><a id="border" href="#">More Shop Demos</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Mega Shop</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Cute Shop</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Sport Shop</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Vendor Shop</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Parallax Shop</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Big Sale</a></li>
+                                <li class="cot-menu"><a href="#">Sale Countdown</a></li>
+                            </div>
+                            <div class="col-md-4">
+                                <li class="cot-menu"><a id="border" href="#">Business Demos</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Agency</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Corporate</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Freelancer</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Explore</a></li>
+                                <li class="cot-menu"><a href="#">Lifestyle</a></li>
+                            </div>
+
+                        </ul>
+                    </div>
+                </li>
+                <li class="dropdown 1" id="hh"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Features <span
+                            class="caret"></span></a>
+                    <div class="col-md-3">
+                        <ul class="dropdown-menu">
+                            <div class="col-md-4">
+                                <li class="cot-menu"><a id="border" href="#">Features</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Core Features</a></li>
+                                <li class="cot-menu"><a id="border" href="#">What’s New</a></li>
+                                <li class="cot-menu"><a id="border" href="#">UX Builder</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Header Designer</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Flatsome Studio</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Fast Performance</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Parallax Scrolling</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Single Page Navigation</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Sticky Sections</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Getting Started</a></li>
+                                <li class="cot-menu"><a  href="#">Footer Features</a></li>
+                            </div>
+                            <div class="col-md-4">
+                                <li class="cot-menu"><a id="border"  href="#">Page 1-1</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Page 1-2</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Page 1-3</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Page 1-2</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Page 1-3</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Page 1-2</a></li>
+                                <li class="cot-menu"><a id="border" href="#">Page 1-3</a></li>
+                            </div>
+                            <div class="col-md-4">
+                                <img src="images/1.jpg" alt=""/>
+                            </div>
+                        </ul>
+                    </div>
+                </li>
+                <li class="dropdown 2" id="hh"><a class="dropdown-toggle" id="ss" data-toggle="dropdown" href="#">Shop <span
+                            class="caret"></span></a>
+                    <div class="col-md-3">
+                        <ul class="dropdown-menu">
+                            <div class="col-md-6">
+                                <li class="cot-menu"><a href="#">Page 1-1</a></li>
+                                <li class="cot-menu"><a href="#">Page 1-2</a></li>
+                                <li class="cot-menu"><a href="#">Page 1-3</a></li>
+                            </div>
+                            <div class="col-md-6">
+                                <li class="cot-menu"><a href="#">Page 1-1</a></li>
+                                <li class="cot-menu"><a href="#">Page 1-2</a></li>
+                                <li class="cot-menu"><a href="#">Page 1-3</a></li>
+                            </div>
+                        </ul>
+                    </div>
+                </li>
+                <li class="dropdown 3" id="hh"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Pages<span
+                            class="caret"></span></a>
+                    <div class="col-md-3">
+                        <ul class="dropdown-menu">
+                            <div class="col-md-12">
+                                <li class="cot-menu"><a href="#">Page 1-1</a></li>
+                                <li class="cot-menu"><a href="#">Page 1-2</a></li>
+                                <li class="cot-menu"><a href="#">Page 1-3</a></li>
+                            </div>
+                        </ul>
+                    </div>
+                </li>
+                <li><a class="dropdown-toggle " id="hh" href="#">Blog</a></li>
+                <li><a class="dropdown-toggle" id="hh" href="#">Elements</a></li>
+            </ul>
+            <div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="#">Demos</a>
+                <a href="#">Features</a>
+                <a href="#">Shop</a>
+                <a href="#">About</a>
+                <a href="#">Pages</a>
+            </div>
+
+
+            <div id="main">
+                <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; </span>
+            </div>
+        </nav>
+
+    </div>
+
+</div>
+</div>
+</div>
+
+<script>
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+</script>
---
+++ modul/10/js/10.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+    }
+
+    function closeNav() {
+        document.getElementById("myNav").style.width = "10%";
+    }
+});
