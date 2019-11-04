26e307100f4684a0f86aadd19d52bdfd49ec65e6
trungcap <ctxtrung@gmail.com>
2019-11-04 21:27:06 +0700

    f

--- Themes+phancong/trungcap/footer.php
+++
@@ -1,2 +0,0 @@
-<?php get_template_part('modul/8','content') ?>
-<?php get_template_part('modul/9','content') ?>
--- Themes+phancong/trungcap/templates/shop.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    /*template name:shop*/
-    
-?>
-<?php get_header(); ?>
-
-<?php get_template_part('modul/11-12','content') ?>;
-<?php get_footer();?>
