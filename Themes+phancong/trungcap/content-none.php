5397aea2b188d4c47be29c22da68c419701d886c
trungcap <ctxtrung@gmail.com>
2019-11-04 21:40:10 +0700

    f

--- Themes+phancong/trungcap/content-none.php
+++
@@ -1,4 +0,0 @@
-<div class="no-post">
-    <?php _e('Nothing post found','trungcap'); ?>
-</div>
-
--- Themes+phancong/trungcap/templates/about.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    /*template name:ABOUT*/
-    
-?>
-<?php get_header(); ?>
-
-<?php get_template_part('modul/14','content') ?>;
-<?php get_footer();?>
